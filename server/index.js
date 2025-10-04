cc1642b6048dc3847eda9cbdf9cbed82f6a06ba8
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-04 11:53:47 -0700

    created a select all categories function and url endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { selectItemsByCategory, selectItemById } = require('./mysqlConnection/items.js');
+const { selectCategory } = require('./mysqlConnection/categories.js');
 const { allNumbers } = require('./regex/regex.js');
 
 console.log(app);
@@ -11,7 +12,13 @@ app.get('/items/item/:id', async (req, res) => {
     try {
         if(req.params.id.match(allNumbers)) {
             const results = await selectItemById(req.params.id);
-            res.send(results);
+            console.log(results);
+            if(results) {
+                res.send(results);
+            }
+            else {
+                res.sendStatus(404).json({error: 'Not Found'});
+            }
         }
         else {
             res.sendStatus(404).json({ error: 'Not Found' });
@@ -26,7 +33,12 @@ app.get('/items/:id', async (req, res) => {
         if(req.params.id.match(allNumbers)) {
             const results = await selectItemsByCategory(req.params.id);
             console.log(results);
-            res.send(results);
+            if(results.length) {
+                res.send(results);
+            }
+            else {
+                res.sendStatus(404).json({ error: 'Not Found' });
+            }
         }
         else {
             res.sendStatus(404).json({ error: 'Not Found' });
@@ -35,6 +47,21 @@ app.get('/items/:id', async (req, res) => {
         console.log(error.message);
     }
 });
+
+app.get('/categories', async (req, res) => {
+    try {
+        const results = await selectCategory();
+        console.log(results);
+        if(results.length) {
+            res.send(results);
+        }
+        else {
+            res.sendStatus(404).json({ error: 'Not Found' });
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 app.listen(8080, () => {
     console.log('server listening on port 8080');
---
+++ server/mysqlConnection/categories.js
@@ -0,0 +1,15 @@
+const pool = require('./connection.js');
+
+const selectCategory = async () => {
+    try {
+        const [results, fields] = await pool.query(
+            `SELECT * FROM categories`
+        );
+        console.log(results);
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = { selectCategory };
