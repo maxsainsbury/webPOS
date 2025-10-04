fe60108628ac142160f5950569624a97a56ba90a
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-04 11:03:02 -0700

    updated functions in items.js to return data from database

--- server/mysqlConnection/items.js
+++ server/mysqlConnection/items.js
@@ -1,17 +1,45 @@
-const mysql = require('mysql2/promise');
 const pool = require('./connection.js');
+const {intToBool} = require('../helpers/helperFunctions.js');
 
-const selectItems = async () => {
+
+const selectItemsByCategory = async (categoryId) => {
     try {
         const [results, fields] = await pool.query(
-            'SELECT * FROM `items`'
+            'SELECT * FROM `items` WHERE `category_id`=?',
+            [categoryId]
         );
-
-        console.log(results);
-        console.log(fields);
+        for(let i = 0; i < results.length; i++) {
+            results[i].is_available = intToBool(results[i].is_available[0]);
+        }
+        return results;
     } catch (error) {
         console.log(error.message);
     }
 }
 
-module.exports = {selectItems};
+const selectItemById = async (itemId) => {
+    try {
+        const [results, fields] = await pool.query(
+            'SELECT * FROM `items` WHERE `id`=?',
+            [itemId]
+        );
+        results[0].is_available = intToBool(results[0].is_available[0]);
+        return results[0];
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const addItem = async (item) => {
+    try {
+        const [results, fields] = await pool.query(
+            'INSERT INTO `items` (name, category_id, price, tax_id)' +
+            'VALUES (?, ?, ?, ?, ?)',
+            [item.name, item.categoryId, item.price, item.taxId]
+        );
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = {selectItemsByCategory, selectItemById};
