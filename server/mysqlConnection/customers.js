7e953380f7d6258a2114a917c930cd5a8a634155
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-05 20:49:33 -0700

    minor visual code fixes

--- server/mysqlConnection/customers.js
+++ server/mysqlConnection/customers.js
@@ -16,9 +16,9 @@ const addCustomer = async (customer) => {
     try {
         customer.postal_code = customer.postal_code.replaceAll(' ', '');
         const [results] = await pool.query(
-            'INSERT INTO customers ' +
-            '(f_name, l_name, phone, address_line1, address_line2, email, city, provence, postal_code, delivery_instructions, credit) ' +
-            'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            `INSERT INTO customers 
+             (f_name, l_name, phone, address_line1, address_line2, email, city, provence, postal_code, delivery_instructions, credit)
+             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
             [customer.f_name, customer.l_name, customer.phone, customer.address_line1, customer.address_line2, customer.email, customer.city, customer.provence, customer.postal_code, customer.delivery_instructions, customer.credit]
         );
         return results;
@@ -31,9 +31,9 @@ const updateCustomer = async (customer) => {
     try {
         customer.postal_code = customer.postal_code.replaceAll(' ', '');
         const [results] = await pool.query(
-            'UPDATE customers ' +
-            'SET f_name = ?, l_name = ?, phone = ?, address_line1 = ?, address_line2 = ?, email = ?, city = ?, provence = ?, postal_code = ?, delivery_instructions = ?, credit = ? ' +
-            'WHERE customer_id = ?',
+            `UPDATE customers
+             SET f_name = ?, l_name = ?, phone = ?, address_line1 = ?, address_line2 = ?, email = ?, city = ?, provence = ?, postal_code = ?, delivery_instructions = ?, credit = ?
+             WHERE customer_id = ?`,
             [customer.f_name, customer.l_name, customer.phone, customer.address_line1, customer.address_line2, customer.email, customer.city, customer.provence, customer.postal_code, customer.delivery_instructions, customer.credit, customer.customer_id]
         );
         return results;
