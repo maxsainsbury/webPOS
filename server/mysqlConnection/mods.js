189b52238f01eeb76d88df5a2b96183358ea9894
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-05 19:32:51 -0700

    created a way to search for mods for a specific item, and the default mods on a item

--- server/mysqlConnection/mods.js
+++ server/mysqlConnection/mods.js
@@ -16,4 +16,28 @@ const selectModsByCategory = async (categoryId) => {
     }
 }
 
-module.exports = { selectModsByCategory }
+const selectModsByItem = async (itemId) => {
+    try {
+        const [results, fields] = await pool.query(
+            'SELECT * FROM mods WHERE mod_id = (SELECT mod_id FROM item_mod WHERE item_id = ?)',
+            [itemId]
+        );
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const selectModsByItemDefault = async (itemId) => {
+    try {
+        const [results, fields] = await pool.query(
+            'SELECT * FROM mods WHERE mods_id = (SELECT mod_id FROM item_mod WHERE item_id = ? AND is_default = 1)',
+            [itemId]
+        )
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = { selectModsByCategory, selectModsByItem, selectModsByItemDefault };
