649190ed719e55404b523c78f41f0ae253b43f5c
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-05 20:49:54 -0700

    added the functionality to add and update mods

--- server/mysqlConnection/mods.js
+++ server/mysqlConnection/mods.js
@@ -31,7 +31,7 @@ const selectModsByItem = async (itemId) => {
 const selectModsByItemDefault = async (itemId) => {
     try {
         const [results, fields] = await pool.query(
-            'SELECT * FROM mods WHERE mods_id = (SELECT mod_id FROM item_mod WHERE item_id = ? AND is_default = 1)',
+            'SELECT * FROM mods WHERE mod_id = (SELECT mod_id FROM item_mod WHERE item_id = ? AND is_default = 1)',
             [itemId]
         )
         return results;
@@ -40,4 +40,32 @@ const selectModsByItemDefault = async (itemId) => {
     }
 }
 
-module.exports = { selectModsByCategory, selectModsByItem, selectModsByItemDefault };
+const addMod = async (mod) => {
+    try {
+        const [results] = await pool.query(
+            `INSERT INTO mods 
+             (mod_name, category_id, is_available)
+             VALUE (?, ?, ?)`,
+            [mod.mod_name, mod.category_id, mod.is_available]
+        );
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const updateMod = async (mod) => {
+    try {
+        const [results] = await pool.query(
+            `UPDATE mods
+            SET mod_name = ?, category_id = ?, is_available = ? 
+            WHERE mod_id = ?`,
+            [mod.mod_name, mod.category_id, mod.is_available, mod.mod_id]
+        );
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = { selectModsByCategory, selectModsByItem, selectModsByItemDefault, addMod, updateMod };
