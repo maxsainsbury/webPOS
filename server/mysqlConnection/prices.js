df5cc6292af2ecf3f1def65eef86e288f6b114fa
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-05 20:53:50 -0700

    updated prices.js to conform to the new database structure

--- server/mysqlConnection/prices.js
+++ server/mysqlConnection/prices.js
@@ -3,7 +3,7 @@ const pool = require('./connection.js');
 const selectPriceByCategory = async (categoryId) => {
     try {
         const [results, fields] = await pool.query(
-            'SELECT * FROM prices WHERE price_id = (SELECT price_id FROM price_category WHERE category_id = ?)',
+            'SELECT * FROM prices WHERE category_id = ?',
             [categoryId]
         );
         return results;
