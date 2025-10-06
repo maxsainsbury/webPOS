1d92c7a092c1a7e48f1b11142610c4d2c13c5df8
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-10-05 20:54:32 -0700

    added the functionality to search for tax rates by category

---
+++ server/mysqlConnection/taxRates.js
@@ -0,0 +1,15 @@
+const pool = require('./connection.js');
+
+const selectTaxRateByCategory = async (categoryId) => {
+    try {
+        const [results, fields] = await pool.query(
+            `SELECT * FROM tax_rates WHERE category_id = ?`,
+            [categoryId]
+        );
+        return results;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = { selectTaxRateByCategory };
