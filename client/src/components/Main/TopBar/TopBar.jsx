5e83ad887a04628548f57b9b30fd1fcc4d30bbd7
Max Sainsbury <maxwell.sainsbury@gmail.com>
2026-02-07 14:11:37 -0800

    fixed errors and modified some values to make the buttons appear better in there seperate uses

--- client/src/components/Main/TopBar/TopBar.jsx
+++ client/src/components/Main/TopBar/TopBar.jsx
@@ -1,11 +1,50 @@
 import './TopBar.css';
 import TouchBtn from "../../Universal/TouchBtn/TouchBtn.jsx";
+import { useEffect, useState } from "react";
+import { appWindow } from '@tauri-apps/api/window';
 
 const TopBar = () => {
-    const btnSize = 'auto';
+
+    let [fullscreenActive, setFullscreenActive] = useState(false);
+
+    const testFunction = () => {
+        console.log("testFunction");
+    }
+
+    const toggleFullscreen = async () => {
+        setFullscreenActive(!fullscreenActive);
+        try {
+            await appWindow.maximize();
+            await appWindow.setFullscreen(!fullscreenActive);
+        } catch (error) {
+            console.error("Failed to toggle fullscreen", error);
+        }
+    }
+
+    const btnNames = [
+        {name: 'test1', func: testFunction},
+        {name: 'test2', func: testFunction},
+        {name: 'test3', func: testFunction},
+        {name: 'test4', func: testFunction},
+        {name: 'test5', func: testFunction},
+        {name: 'test6', func: testFunction},
+        {name: 'test7', func: testFunction},
+        {name: 'test8', func: testFunction},
+        {name: 'test9', func: testFunction},
+        {name: 'test10', func: testFunction},
+        {name: 'test11', func: testFunction},
+        {name: 'Fullscreen', func: toggleFullscreen},];
+
     return (
         <div id="topbar">
-            <TouchBtn width={btnSize} className='rectangle topbarBtn' />
+            {btnNames.map(({name, func}, index) => (
+                <TouchBtn
+                    key={index}
+                    name={name}
+                    onClick={func}
+                    className="rectangle topbarBtn"
+                />
+            ))}
         </div>
     )
 }
