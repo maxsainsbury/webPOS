6068322c88680d01b7e1308caf9173b083c2895d
Max Sainsbury <maxwell.sainsbury@gmail.com>
2026-02-06 18:52:15 -0800

    made it so the tauri window goes fullscreen on start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
+import { appWindow } from '@tauri-apps/api/window';
 import './App.css'
 import LoginPanel from "./components/Login/LoginPanel/LoginPanel.jsx";
 
-import { invoke } from "@tauri-apps/api";
-
 function App() {
+
+    useEffect(() => {
+        appWindow.maximize();
+    }, []);
 
   return (
     <>
