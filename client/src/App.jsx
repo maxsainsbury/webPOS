adec1f93eb5fef036b3b1c9d0033bb48df5da279
Max Sainsbury <maxwell.sainsbury@gmail.com>
2026-02-07 17:00:37 -0800

    removed temporary import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import LoginPanel from "./components/Login/LoginPanel/LoginPanel.jsx";
 import MainPanel from "./components/Main/MainPanel/MainPanel.jsx";
 
-import {getTauriVersion} from '@tauri-apps/api/app';
-
 function App() {
 
     const [user, setUser] = useState(null);
